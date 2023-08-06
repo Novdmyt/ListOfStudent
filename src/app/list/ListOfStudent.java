b36ce96e611dfc999965c72be04634e31cfe8e84
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-06 20:24:59 +0300

    Add method getData

---
+++ src/app/list/ListOfStudent.java
@@ -0,0 +1,28 @@
+package app.list;
+
+import app.student.Student;
+import app.util.Speciality;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListOfStudent {
+    private List<Student> getData(){
+        List<Student> students = Arrays.asList(
+                new Student("Alex", Speciality.Physics, 1),
+                new Student("Rika", Speciality.Biology, 4),
+                new Student("Julia", Speciality.Biology, 2),
+                new Student("Steve", Speciality.History, 4),
+                new Student("Mike", Speciality.Finance, 1),
+                new Student("Hinata", Speciality.Biology, 2),
+                new Student("Richard", Speciality.History, 1),
+                new Student("Kate", Speciality.Psychology, 2),
+                new Student("Sergey", Speciality.ComputerScience, 4),
+                new Student("Maximilian", Speciality.ComputerScience, 3),
+                new Student("Tim", Speciality.ComputerScience, 5),
+                new Student("Ann", Speciality.Psychology, 1)
+        );
+
+        return students;
+    }
+}
